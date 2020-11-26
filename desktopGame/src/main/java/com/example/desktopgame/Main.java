d87a58a2da8cf439834cf4ad2c82d13be04de5fe
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2020-11-26 21:18:24 +0100

    Estructura definitiva PC

--- desktopGame/src/main/java/com/example/desktopgame/Main.java
+++ desktopGame/src/main/java/com/example/desktopgame/Main.java
@@ -1,20 +1,51 @@
 package com.example.desktopgame;
 
+import com.example.engine.Engine;
 import com.example.logica.Logica;
+import com.example.pcengine.PCGraphics;
 import com.example.pcengine.PcEngine;
 
 import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
 
 
 public class Main {
     public static void main(String[] args) throws Exception
     {
-        MyJFrame frame = new MyJFrame();
+        Engine engine;
+        PCGraphics _graphics;
+        Logica _logic;
+        boolean _exit = false;
+
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        frame.setSize(640,480);
         frame.setIgnoreRepaint(true);
         frame.createBufferStrategy(2);
         BufferStrategy strat = frame.getBufferStrategy();
 
-        frame.run();
+
+
+       _logic = new Logica();
+        engine = new PcEngine(_logic,frame);
+        _logic.getEngine(engine);
+
+        try {
+            engine.running();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while(!_exit) {
+        }
+
     }
+
+
+
 
 }
